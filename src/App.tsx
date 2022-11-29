7ee864798548457daa9e34c4b550e4f934b5fd00
Astemir Bekov <artisticheski@gmail.com>
2022-11-29 19:40:29 +0300

    added select

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
-import { pages } from './helpers';
-import { Forms } from './pages';
-import { EThemes } from './store/models/main';
+import {pages} from './helpers';
+import {Forms} from './pages';
+import {EThemes} from './store/models/main';
+import logo from "./images/Group1.svg"
 
 function App() {
   const [theme, setTheme] = useState(EThemes.Classic);
@@ -12,23 +13,34 @@ function App() {
   const handleChangeTheme = (event: ChangeEvent<HTMLSelectElement>) => {
     setTheme(event.target.value as EThemes)
   }
-  
-  return (
-    <div className="App">
-      <select onChange={handleChangeTheme} value={theme}>
-        {pages.map((page) => (
-          <option
-            key={page.value}
-            value={page.value}
-            selected={theme === page.value}
-          >
-            {page.name}
-          </option>
-        ))}
-      </select>
 
-      <Form />
-    </div>
+  return (
+      <div className="App">
+        <div className="selector_box">
+          <img src={logo} alt="logo"/>
+          <div className="selectTheme">
+            <button>Классика</button>
+            <button>ЧБ</button>
+            <button>Золото</button>
+            <button>Национальный</button>
+            <button>Модерн</button>
+            <button>Акварель</button>
+          </div>
+          <a className="mobilePhone" href="tel:+79999999999">+7999 999-99-99</a>
+        </div>
+        {/*<select onChange={handleChangeTheme} value={theme}>*/}
+        {/*  {pages.map((page) => (*/}
+        {/*    <option*/}
+        {/*      key={page.value}*/}
+        {/*      value={page.value}*/}
+        {/*      selected={theme === page.value}*/}
+        {/*    >*/}
+        {/*      {page.name}*/}
+        {/*    </option>*/}
+        {/*  ))}*/}
+        {/*</select>*/}
+        <Form/>
+      </div>
   );
 }
 
